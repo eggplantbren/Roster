d89065bfffbdbd39ee20dbebce3797faeaaf529f
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-02-22 12:43:35 +1300

    a menu

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from database import *
 
+def main_menu():
+    """
+    Display the main menu. Return the selected option.
+    """
+    print("[T] View or manage tasks.")
+    print("[P] View or manage people.")
+
+    print("\n[Q] Quit the program.\n")
+    response = input("Enter your choice: ")
+    return response.lower()
+
 create_tables()
+
+
+while True:
+    response = main_menu()
+    if response == "q":
+        break
+
 close_connection()
 
